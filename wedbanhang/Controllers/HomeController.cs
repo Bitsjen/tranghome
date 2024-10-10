6ac97af4fba41a2ed8026f5c3231c5463dabed6b
Bitsjen <dangsuper2005@gmail.com>
2024-10-10 11:24:49 +0700

    aa

--- wedbanhang/Controllers/HomeController.cs
+++ wedbanhang/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace wedbanhang.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application n page.";
 
             return View();
         }
